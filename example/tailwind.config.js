c95312406b9556e9a96e6fb84f411cb32087355c
YOLO-GIT <ahmadsufi345@gmail.com>
2024-03-13 12:34:56 +0800

    Config JS

---
+++ example/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./bolls/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'parallax': 'url("../bolls/Another Design/banner.jpg")',
+        'parallax_2': 'url("../bolls/Another Design/banner.jpg")'
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
